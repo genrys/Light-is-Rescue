293629bb88e405eeb5b14bb6ae41238efee71660
genrys <genrys@github.com>
2016-03-02 17:00:08 +0200

    Player attack created file

---
+++ Project/New Unity Project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int curHealth;
+	public int startHealth = 100;
+	public Slider healthSlider; 
+	public Image damageImage;
+	public float flashSpeed = 5f;   
+	public Color flashColor = new Color(1f, 0f, 0f, 0.1f);  
+
+	PlayerCharachter playerCharachter;
+	bool isDead;
+	bool damaged;
+
+
+	void Awake(){
+	
+		playerCharachter = GetComponent<PlayerCharachter> ();
+
+		curHealth = startHealth;
+
+	}
+
+	void Update(){
+
+		if (damaged) {
+			
+			damageImage.color = flashColor;
+
+		}
+		else {
+
+			flashColor = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
+	}
+
+	public void TakeDamage (int amount)
+	{
+
+		damaged = true;
+
+		curHealth -= amount;
+
+		healthSlider.value = curHealth;
+
+		if (curHealth <= 0 && !isDead) {
+
+			Death ();
+
+		}
+
+	}
+
+	void Death(){
+
+		isDead = true;
+
+		playerCharachter.enabled = false;
+
+	}
+}
+
+
